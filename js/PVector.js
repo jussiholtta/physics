90f1034a69b5b6813db43fae3192eb0faee320bc
jussiholtta <jussi.holtta@gmail.com>
2016-04-14 20:54:39 +0300

    added cross product, unit vector and scaling vectors

--- js/PVector.js
+++ js/PVector.js
@@ -10,9 +10,25 @@ physicsApp.Vector.prototype.length = function() {
   return Math.sqrt(Math.pow(this.a,2) + Math.pow(this.b,2) + Math.pow(this.b,2));
 }
 
+physicsApp.Vector.prototype.unit = function() {
+  var l = this.length();
+  return new physicsApp.Vector(this.a/l, this.b/l, this.c/l);
+}
+
 physicsApp.Vector.prototype.plus = function(v) {
   if( v instanceof physicsApp.Point)
     throw new Error("No, no, no! Don't add points to vectors!");
   return new physicsApp.Vector(this.a + v.a, this.b + v.b, this.c + v.c);
+}
+
+physicsApp.Vector.prototype.product = function(v) {
+  var i = this.b*v.c - this.c*v.b;
+  var j = this.c*v.a - this.a*v.c;
+  var k = this.a*v.b - this.b*v.a;
+  return new physicsApp.Vector(i,j,k);
+}
+
+physicsApp.Vector.prototype.scale = function(s) {
+  return new physicsApp.Vector(this.a*s, this.b*s, this.c*s);
 }
 
--- test/PVector-test.js
+++ test/PVector-test.js
@@ -5,6 +5,15 @@ describe("physicsApp.PVector", function() {
     var vector2 = new physicsApp.Vector(1,2,2);
     var point = new physicsApp.Point(0,0,0);
 
+    //unit vectors
+    var iv = new physicsApp.Vector(1,0,0),
+    jv = new physicsApp.Vector(0,1,0),
+    kv = new physicsApp.Vector(0,0,1),
+    ivn = new physicsApp.Vector(-1,0,0),
+    jvn = new physicsApp.Vector(0,-1,0),
+    kvn = new physicsApp.Vector(0,0,-1);
+
+
     beforeEach(function(){
       });
 
@@ -18,13 +27,13 @@ describe("physicsApp.PVector", function() {
       });
 
     it("should match equal vectors", function() {
-      expect(vector).toEqual(vector2);
-      });
+        expect(vector).toEqual(vector2);
+        });
 
     it("shouldn't match unequal vectors", function() {
-      vector2.a = 14;
-      expect(vector).not.toEqual(vector2);
-      });
+        vector2.a = 14;
+        expect(vector).not.toEqual(vector2);
+        });
 
     it("should calculate length", function() {
         expect(vector.length()).toEqual(3);
@@ -35,16 +44,48 @@ describe("physicsApp.PVector", function() {
     //  expect(vector
     //});
 
-    it("should be able to sum vectors", function() {
+    it("should be able to sum 2 vectors", function() {
         var sumVector = vector.plus(vector2);
         expect(sumVector).toEqual(new physicsApp.Vector(2,4,4)); 
         });
 
+    it("should be able to chain sum vectors", function() {
+        var sumVector = iv.plus(jv).plus(kv).plus(ivn);
+        expect(sumVector).toEqual(new physicsApp.Vector(0,1,1)); 
+        });
+
     it("shouldn't allow adding a point to a vector", function() {
-      expect( function(){ vector.plus(point); } ).toThrow(new Error("No, no, no! Don't add points to vectors!"));
+        expect( function(){ vector.plus(point); } ).toThrow(new Error("No, no, no! Don't add points to vectors!"));
+        });
+
+    it("should do vector products", function() {
+        expect(iv.product(jv)).toEqual(kv);
+        expect(jv.product(iv)).toEqual(kvn);
+
+        expect(iv.product(kv)).toEqual(jvn);
+        expect(kv.product(iv)).toEqual(jv);
+
+        expect(jv.product(kv)).toEqual(iv);
+        expect(kv.product(jv)).toEqual(ivn);
+        });
+
+    it("should be able to chain vector products", function() {
+        expect(iv.product(kv).product(kvn)).toEqual(new physicsApp.Vector(1,0,0));
       });
 
+    it("should get 0 vector for self product", function() {
+        expect(iv.product(iv)).toEqual(new physicsApp.Vector(0,0,0));
+      });
 
-//it("", function() {
+    it("should be able to get unit vector for longer vector", function() {
+        var l = vector.length();
+        expect(vector.unit()).toEqual(new physicsApp.Vector(1/l,2/l,2/l));
+      });
+
+    it("should be able to scale vectors", function() {
+      expect(vector.scale(2)).toEqual(new physicsApp.Vector(2,4,4));
+    });
+    
+    //it("", function() {
     //});
 });
